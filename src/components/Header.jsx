7bfe5b460bf7ae4b3e1ebed99cdce564f3cecdae
Michael Wells <mwells1286@gmail.com>
2020-03-22 21:02:35 -0700

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+function Header() {
+  return <div className="header">
+
+      <NavigationBar />
+  </div>;
+}
+
+export default Header;
