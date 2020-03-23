ab3ed9281bfe8d632ddf4b4bf81e9fe8906d3ea3
Michael Wells <mwells1286@gmail.com>
2020-03-22 21:06:04 -0700

    add resume data

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Michael Wells",
+  role: "Full-Stack Developer",
+  email: "me@mikeywayne.dev",
+  roleDescription:
+    "Motivated and passionate junior developer with a detailed professional history revolving around community, curiosity, and creativity.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "www.linkedin.com/in/michael-wayne-wells",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/Michael-Wayne-Wells",
+      className: "fa fa-github"
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/stabby_lambda",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "As a recent graduate from Epicodus, I am currently continuing to grow as a full-stack developer with a focus on Ruby on Rails and React. In my free time, I am learning deployment with Kubernetes and Docker and topics in information security. I'm looking to bring 15 years of experience in the food and beverage industry, combined with my broad range of skills developed as a small business owner, and my attention to detail as an editor for an academic publishing platform to grow as a web developer and computer programmer. ",
+  address: "Portland, Or",
+  website: "https://rbhatia46.github.io",
+  education: [
+    {
+      universityName: "Epicodus",
+      specialization: "React & Ruby on Rails",
+      yearStar: "2019",
+      yearOfPassing: "2020"
+    },
+    {
+      universityName: "New Centre for Research and Practice",
+      specialization: "Critical Philosophy",
+      yearStart: "2017",
+      yearOfPassing: "2019"
+    },
+    {
+      universityName: "Noma",
+      specialization: "Nordic Gastronomy Internship",
+      yearStart: "2012",
+      yearOfPassing: "2012"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "Javascript"
+    },
+    {
+      skillname: "Ruby on Rails"
+    },
+    {
+      skillname: "Reactjs"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
