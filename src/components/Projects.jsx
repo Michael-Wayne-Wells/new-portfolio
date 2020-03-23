9e403bcd2b13a7923496f2226d730a4cf12d5f94
Michael Wells <mwells1286@gmail.com>
2020-03-22 21:03:30 -0700

    add project components

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="projects">
+      <style jsx>{`
+        .projects {
+          background-color: blue;
+          height: 500px;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Projects;
