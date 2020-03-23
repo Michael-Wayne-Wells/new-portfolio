57b69c8194deaa71c4bf9ed84d4a2befc5225c91
Michael Wells <mwells1286@gmail.com>
2020-03-22 21:02:14 -0700

    add contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact(){
+    return(
+        <div className="contact">
+            <style jsx>{`
+                .contact {
+                    background-color: black;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export default Contact
