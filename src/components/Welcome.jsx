8a97527aade8f2a6a766ad8d774ed61a39d83836
Michael Wells <mwells1286@gmail.com>
2020-03-24 19:57:51 -0700

    add profile image

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,6 +4,19 @@ function Welcome() {
   return (
     <div className="welcome">
       <Goo />
+      <style jsx>{`
+        .profileImg {
+          width: 300px;
+          height: auto;
+          margin: 0 auto;
+        }
+      `}</style>
+      <h1>Welcome!</h1>
+      <img
+        className="profileImg"
+        src="profile.png"
+        alt="michael wells profile illustration"
+      />
     </div>
   );
 }
